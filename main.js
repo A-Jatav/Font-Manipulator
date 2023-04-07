5afaa0fa8c0c8daca7130b2294fb650a74319d55
A-Jatav <108002072+A-Jatav@users.noreply.github.com>
2023-04-07 11:21:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+var leftWristX = 0;
+var rightWristX = 0;
+
+
 function preload() {
 };
 
@@ -14,7 +18,18 @@ function modelLoaded(){
 };
 
 function draw(){
+    var sizee = Math.floor(leftWristX - rightWristX);
+    console.log(sizee);
+    fill("green");
+    textSize(sizee);
+    text("Apratim Jatav", 250, 225);
+    document.getElementById("font_size").innerHTML = sizee;
 };
 
 function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+    };
 };
